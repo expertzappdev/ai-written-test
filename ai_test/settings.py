62b27cbd21b378c216fd63fc181d07f02d73e384
Shweta Ladne <shweta.ladne.averybit@gmail.com>
2025-10-29 20:03:30 +0530

    update settings file

--- ai_test/settings.py
+++ ai_test/settings.py
@@ -194,4 +194,5 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
